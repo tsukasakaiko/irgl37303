f97d7a60450f50dbe116d0f981b6f72e03fb6bdb
tsukasakaiko <kaikobt825@gmail.com>
2022-04-02 21:16:17 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 2022_02_18_165941) do
   end
 
   create_table "irgls", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "text"
-    t.text "image"
+    t.string "spot", null: false
+    t.string "ship_no", null: false
+    t.string "status", null: false
+    t.string "remarks", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
@@ -31,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_02_18_165941) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
